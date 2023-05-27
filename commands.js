170800a370944e6a8d7191ce83b3a3b808f3acfd
yagami-light007 <134776106+yagami-light007@users.noreply.github.com>
2023-05-27 15:24:29 +0530

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,40 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function sigmaculate() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operator = document.getElementById("operator").value;
+    var result;
+
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            result = "INVALID";
+    }
+    document.getElementById("result").innerHTML = result;
+}
